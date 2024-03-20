2b572453090c4f0f4861c5820aa009c0a6493222
hlebu5hek <74304719+hlebu5hek@users.noreply.github.com>
2024-03-20 18:20:36 +0400

    Create lab.py

---
+++ lab.py
@@ -0,0 +1,145 @@
+'''Вариант 16
+С клавиатуры вводится два числа K и N. Квадратная матрица А(N,N),
+состоящая из 4-х равных по размерам подматриц, B,C,D,E заполняется
+случайным образом целыми числами в интервале [-10,10].
+Для тестирования использовать не случайное заполнение, а целенаправленное.
+d e
+c b
+Для простоты все индексы в подматрицах относительные.
+По сформированной матрице F (или ее частям) необходимо вывести не менее 3 разных графиков.
+Программа должна использовать функции библиотек numpy  и mathplotlib #print(int(np.linalg.det(np.array(a))))
+Формируется матрица F следующим образом: скопировать в нее А и  если в Е
+максимальный элемент в нечетных столбцах больше, чем сумма чисел в
+нечетных строках, то поменять местами С и В симметрично, иначе В и Е
+поменять местами несимметрично. При этом матрица А не меняется.
+После чего если определитель матрицы А больше суммы диагональных элементов матрицы
+F, то вычисляется выражение: A-1*AT – K * F-1, иначе вычисляется выражение
+(AТ +G-FТ)*K, где G-нижняя треугольная матрица, полученная из А. Выводятся по мере
+формирования А, F и все матричные операции последовательно.
+'''
+from random import randint as rnd
+import numpy as np
+import matplotlib as mpl
+
+def printList(z):
+    for i in z:
+        for j in i:
+            print("{:4}".format(j), end=' ')
+        print()
+    print()
+
+k, n = int(input("k = ")), int(input("n = "))
+m = n//2
+n = m*2
+a = []
+
+for i in range(n):
+    a.append([])
+    for j in range(n):
+        a[i].append(rnd(-10,10))
+
+a_arr = np.array(a)
+print("Matrix A : ")
+printList(a)
+
+b = []
+c = []
+d = []
+e = []
+for i in range(m):
+    b.append([])
+    c.append([])
+    d.append([])
+    e.append([])
+    for j in range(m):
+        b[i].append(a[i+m][j+m])
+        c[i].append(a[i+m][j])
+        d[i].append(a[i][j])
+        e[i].append(a[i][j+m])
+
+print("Matrix B : ")
+printList(b)
+print("Matrix C : ")
+printList(c)
+print("Matrix D : ")
+printList(d)
+print("Matrix E : ")
+printList(e)
+
+min_ = 11
+for i in range(m):
+    for j in range(1, m, 2):
+        min_ = min(min_, e[i][j])
+
+sum_ = 0
+for i in range(1, m, 2):
+    for j in range(m):
+        sum_ += e[i][j]
+
+if min_ > sum_:
+    print("Minimal in odd columns ({:1}) is greater than Summ of odd rows ({:2})\n".format(min_, sum_))
+
+    for i in range(m):
+        for j in range(m):
+            c[i][j], b[i][m-j-1] = b[i][m-j-1], c[i][j]
+else:
+    print("Minimal in odd columns ({:1}) is less than Summ of odd rows ({:2})\n".format(min_, sum_))
+
+    for i in range(m):
+        for j in range(m):
+            b[i][j], e[i][j] = e[i][j], b[i][j]
+
+f = []
+f.extend(d)
+f.extend(c)
+for i in range(m):
+    f[i].extend(e[i])
+for i in range(m, n):
+    f[i].extend(b[i-m])
+
+print("Matrix F : ")
+printList(list(f))
+
+deta = np.linalg.det(a_arr)
+a_arr_tr = a_arr.transpose()
+
+sum_ = 0
+for i in range(n):
+    sum_ += f[i][i]
+
+if deta > sum_: #A-1*AT – K * F-1
+    print("Det A ({:1}) is greater than Summ main diagonal ({:2})\n".format(int(deta), sum_))
+    a_arr_inv_mul_trans = np.matmul(np.linalg.inv(a_arr), a_arr_tr)
+    print("A-1 * AT : ")
+    printList(list(a_arr_inv_mul_trans))
+
+    f_arr_inv = np.linalg.inv(np.array(f))
+    f_arr_inv *= k
+    print("K * F-1 : ")
+    printList(list(f_arr_inv))
+
+    out_ = np.add(a_arr_inv_mul_trans, (-1)*f_arr_inv)
+    print("A-1 * AT – K * F-1 : ")
+    printList(list(out_))
+
+else: #(AТ +G-FТ)*K
+    print("Det A ({:1}) is less than Summ main diagonal ({:2})\n".format(int(deta), sum_))
+    print("AT : ")
+    printList(list(a_arr_tr))
+
+    f_arr_tr = np.array(f)
+    f_arr_tr.transpose()
+    print("FT : ")
+    printList(list(f_arr_tr))
+
+    g = np.tril(a_arr, 0)
+    print("G : ")
+    printList(list(g))
+
+    out_ = np.add(a_arr_tr, g)
+    out_ = np.add(out_, (-1)*f_arr_tr)
+    out_ *= k
+    print("(AТ + G - FТ) * K : ")
+    printList(list(out_))
+
+
